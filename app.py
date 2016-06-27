96d8bb268ca7d3dae430f2da33981572e883e818
Jaiden Mispy <jaiden@mispy.me>
2016-06-27 12:33:09 +1000

    Templates & static assets

--- app.py
+++ app.py
@@ -1,11 +1,24 @@
 import cherrypy
 import os
+from jinja2 import Environment, FileSystemLoader
+env = Environment(loader=FileSystemLoader('html'))
 
 class HelloWorld(object):
+    @cherrypy.expose
     def index(self):
-        return "Hello World!"
-    index.exposed = True
+        tmpl = env.get_template('index.html')
+        data_to_show = ['foo', 'bar', 'etc']
+        return tmpl.render(data=data_to_show);
 
-cherrypy.config.update({'server.socket_host': '0.0.0.0'})
-cherrypy.config.update({'server.socket_port': int(os.environ.get('PORT', '5000'))})
-cherrypy.quickstart(HelloWorld())
+config = {
+    'global': {
+        'server.socket_host': '0.0.0.0',
+        'server.socket_port': int(os.environ.get('PORT', 5000)),
+    },
+    '/assets': {
+        'tools.staticdir.root': os.path.dirname(os.path.abspath(__file__)),
+        'tools.staticdir.on': True,
+        'tools.staticdir.dir': 'assets',
+    }
+}
+cherrypy.quickstart(HelloWorld(), '/', config=config)
